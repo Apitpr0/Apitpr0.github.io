8a8efaa4ba9a15b2f402a4229d1f25e187bf63b1
Apitpr0 <apitpro123@gmail.com>
2025-05-03 03:39:54 +0800

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <a href="#home" className="font-mono text-2xl font-bold text-white">
-            IT Security <span className="text-blue-500">Specialist</span>
+            Invictus<span className="text-blue-500">.MY</span>
           </a>
           <div
             className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
---
+++ src/components/sections/Contact.jsx
@@ -0,0 +1,93 @@
+import emailjs from "emailjs-com";
+import { useState } from "react";
+
+export const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        e.target,
+        import.meta.env.VITE_PUBLIC_KEY
+      )
+      .then((result) => {
+        alert("Message sent successfully!");
+        setFormData({ name: "", email: "", message: "" }); // Reset form fields after successful submission
+      })
+      .catch((error) => {
+        alert("Failed to send message. Please try again later.");
+        console.error("Error sending message: ", error.text); // log error to the console for debugging
+      });
+  };
+
+  return (
+    <section
+      id="contact"
+      className="min-h-screen flex items-center justify-center"
+    >
+      <div className="px-4 w-150">
+        <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+          Get in Touch
+        </h2>
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          <div className="relative">
+            <input
+              type="text"
+              name="name"
+              id="name"
+              required
+              value={formData.name}
+              className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white text-xl transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+              placeholder="Name"
+              onChange={(e) =>
+                setFormData({ ...formData, name: e.target.value })
+              }
+            />
+          </div>
+          <div className="relative">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              required
+              value={formData.email}
+              className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white text-xl transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+              placeholder="Email Address"
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
+            />
+          </div>
+          <div className="relative">
+            <textarea
+              name="message"
+              id="message"
+              required
+              value={formData.message}
+              rows={5}
+              className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white text-xl transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+              placeholder="Your Message"
+              onChange={(e) =>
+                setFormData({ ...formData, message: e.target.value })
+              }
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white rounded px-6 py-3 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
